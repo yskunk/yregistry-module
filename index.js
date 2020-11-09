5668ae6843cbbd4ad8f3f19e8cd70669a6092354
sam <sam@freighttrust.com>
2020-11-08 17:51:38 -0800

    feat(init): inital commit

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * v2 Yield Token Contracts and Yearn Registry Mapper
+ * @property {int} contractAddress - stringified integer with the corresponding ethereum mainnet checksum address
+ */
+
+module.exports = {
+	// Ethereum Mainnet Registry
+	"0": "0x3ee41c098f9666ed2ea246f4d2558010e59d63a0",
+
+	// YFI
+	"1": "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e",
+
+	// YDaiV2 https://github.com/iearn-finance/itoken/blob/master/contracts/YDAIv2.sol
+	"2": "0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01",
+
+	// YUsdcV2 https://github.com/iearn-finance/itoken/blob/master/contracts/YUSDCv2.sol
+	"3": "0xd6aD7a6750A7593E092a9B218d66C0A814a3436e",
+
+	// YUsdtV2 https://github.com/iearn-finance/itoken/blob/master/contracts/YUSDTv2.sol
+	"4": "0x83f798e925BcD4017Eb265844FDDAbb448f1707D",
+
+	// ysUSDv2 https://github.com/iearn-finance/itoken/blob/master/contracts/YSUSDv2.sol
+	"5": "0xF61718057901F84C4eEC4339EF8f0D86D2B45600",
+
+	// yTUSDv2 https://github.com/iearn-finance/itoken/blob/master/contracts/YTUSDv2.sol
+	"6": "0x73a052500105205d34daf004eab301916da8190f",
+};
